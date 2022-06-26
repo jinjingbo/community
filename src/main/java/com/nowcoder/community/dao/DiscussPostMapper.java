e9f40470b3f02d39bf61faba08e44d855e5f9cdd
jinjingbo <jinjingbo1@163.com>
2022-06-26 15:19:23 +0800

    第一次

---
+++ src/main/java/com/nowcoder/community/dao/DiscussPostMapper.java
@@ -0,0 +1,20 @@
+package com.nowcoder.community.dao;
+
+import com.nowcoder.community.entity.DiscussPost;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+
+@Mapper
+public interface DiscussPostMapper {
+//在配置文件 discusspost-mapper.xml中实现方法
+    //返回offer-limit的贴子
+    List<DiscussPost> selectDiscussPosts(int userId, int offset, int limit);
+
+    // @Param注解用于给参数取别名,
+    // 如果只有一个参数,并且在<if>里使用,则必须加别名.
+    //知道id返回处在的总行数
+    int selectDiscussPostRows(@Param("userId") int userId);
+
+}
---
+++ src/main/java/com/nowcoder/community/dao/UserMapper.java
@@ -0,0 +1,23 @@
+package com.nowcoder.community.dao;
+
+import com.nowcoder.community.entity.User;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface UserMapper {
+// userMapper申请数据库信息，在user-mapper.xml中实现sql语句信息。
+    User selectById(int id);
+
+    User selectByName(String username);
+
+    User selectByEmail(String email);
+
+    int insertUser(User user);
+
+    int updateStatus(int id, int status);
+
+    int updateHeader(int id, String headerUrl);
+
+    int updatePassword(int id, String password);
+
+}
